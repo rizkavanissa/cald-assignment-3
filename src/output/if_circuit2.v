578aa62827406771ac95ce08e23f88c14f4af5f0
Nafis <nafisfragern@gmail.com>
2024-04-30 10:58:12 +0700

    on Nafis mac is good

--- src/output/if_circuit2.v
+++ src/output/if_circuit2.v
@@ -17,8 +17,8 @@ module HLSM (Clk, Rst, Start, Done, a, b, c, one, z, x);
     reg signed dLTe; 
     reg signed dEQe; 
     reg signed dLTEe; 
-    reg [2:0] State;
-    parameter Wait = 0, Final = 1, S0 = 2, S1 = 3, S2 = 4, S3 = 5, S4 = 6, S5 = 7, S6 = 8, S7 = 9;
+    reg [1:0] State;
+    parameter Wait = 0, Final = 1, S0 = 2, S1 = 3, S2 = 4, S3 = 5;
 
     always @(posedge Clk) begin
         if (Rst == 1) begin
@@ -37,53 +37,24 @@ module HLSM (Clk, Rst, Start, Done, a, b, c, one, z, x);
                     end
                 end
                 S0: begin
-                    d <= a + b;
-                    e <= a + c;
+                    f <= a - b;
                     State <= S1;
                 end
                 S1: begin
-                    f <= a - b;
+                    d <= a + b;
+                    State <= S2;
+                end
+                S2: begin
+                    e <= a + c;
+                    State <= S3;
+                end
+                S3: begin
                     if (d == e) begin
                         dEQe <= 1;
                     end
                     else begin
                         dEQe <= 0;
                     end
-                    if (d > e) begin
-                        dLTe <= 1;
-                    end
-                    else begin
-                        dLTe <= 0;
-                    end
-                    State <= S2;
-                end
-                S2: begin
-                    dLTEe <= dEQe + dLTe;
-                    State <= S3;
-                end
-                S3: begin
-                    if (dLTEe) begin
-                        if (dLTe) begin
-                            g <= e + one;
-                            h <= f + one;
-                        end
-                        g <= d + e;
-                        h <= f + e;
-                        end
-                    State <= S4;
-                end
-                S4: begin
-                    State <= S5;
-                end
-                S5: begin
-                    State <= S6;
-                end
-                S6: begin
-                    x <= h << one;
-                    State <= S7;
-                end
-                S7: begin
-                    z <= h >> one;
                     State <= Final;
                 end
                 Final: begin
--- src/output/latency_circuit4.v
+++ src/output/latency_circuit4.v
@@ -1,6 +1,6 @@
 `timescale 1ns / 1ps
 
-module HLSM (Clk, Rst, Start, Done, a, b, c, d, e, f, j, k);
+module HLSM (Clk, Rst, Start, Done, a, b, c, d, e, f, g, j, l);
     input Clk, Rst, Start;
     input signed [15:0] a; 
     input signed [15:0] b; 
@@ -8,13 +8,15 @@ module HLSM (Clk, Rst, Start, Done, a, b, c, d, e, f, j, k);
     input signed [15:0] d; 
     input signed [15:0] e; 
     input signed [15:0] f; 
+    input signed [15:0] g; 
     output reg Done;
     output reg signed [15:0] j; 
-    output reg signed [15:0] k; 
+    output reg signed [15:0] l; 
     reg signed [15:0] h; 
     reg signed [15:0] i; 
-    reg [2:0] State;
-    parameter Wait = 0, Final = 1, S0 = 2, S1 = 3, S2 = 4, S3 = 5, S4 = 6, S5 = 7;
+    reg signed [15:0] k; 
+    reg [1:0] State;
+    parameter Wait = 0, Final = 1, S0 = 2, S1 = 3, S2 = 4, S3 = 5;
 
     always @(posedge Clk) begin
         if (Rst == 1) begin
@@ -33,25 +35,20 @@ module HLSM (Clk, Rst, Start, Done, a, b, c, d, e, f, j, k);
                     end
                 end
                 S0: begin
-                    h <= a * b;
+                    h <= a + b;
+                    k <= e * f;
                     State <= S1;
                 end
                 S1: begin
+                    i <= h + c;
                     State <= S2;
                 end
                 S2: begin
-                    i <= h + c;
+                    j <= i + d;
+                    l <= k * g;
                     State <= S3;
                 end
                 S3: begin
-                    State <= S4;
-                end
-                S4: begin
-                    j <= i * d;
-                    k <= e * f;
-                    State <= S5;
-                end
-                S5: begin
                     State <= Final;
                 end
                 Final: begin
